eab1bca591793f39f136ea1e4ef3d27002c43d39
net <nati2mulugeta@gmail.com>
2022-04-13 23:33:02 -0700

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+function HatsPage(props) {
+  return <h1>HATS PAGE {props.match.params.id}</h1>;
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route path="/hats/:id" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,28 +12,33 @@ export default class Directory extends React.Component {
           title: "Hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
+          linkUrl: "hats",
         },
         {
           title: "Jackets",
           imageUrl: "https://i.ibb.co/0jqHpnp/jackets.png",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "Sneakers",
           imageUrl: "https://i.ibb.co/GCCdy8t/sneakers.png",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "Womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "Mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -42,8 +47,8 @@ export default class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.section.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.section.map(({ id, ...otherProps }) => (
+          <MenuItem key={id} {...otherProps} />
         ))}
       </div>
     );
