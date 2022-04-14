855897d905e18c785219ec0bcf91f0981a8e35d6
net <nati2mulugeta@gmail.com>
2022-04-14 05:25:26 -0700

    ahop page added along with item preview and item componentss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { Switch, Route } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shopPage.component";
 
-function HatsPage(props) {
-  return <h1>HATS PAGE {props.match.params.id}</h1>;
-}
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-        <Route path="/hats/:id" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
